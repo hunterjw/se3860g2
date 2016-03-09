b48c58e28f49efdc1b59716331ffa09c5a7a9b30
schwoerera <quinnderkan1994@gmail.com>
2016-03-08 20:43:49 -0600

    File selection
    I got rid of edit as I didn't think we needed it. If we do just add it
    back. Now able to create a file (with nothing in it of course) as the
    file extension of .FHX. Save and load do nothing yet.

--- src/prototype1/FHKFilter.java
+++ src/prototype1/FHKFilter.java
@@ -1,10 +1,11 @@
+package prototype1;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 import java.io.File;
-import javax.swing.*;
 import javax.swing.filechooser.*;
 
 /**
